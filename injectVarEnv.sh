edc4aa10b02ee5010a35d857d37268ab4287a81a
fethca <jay.grijol@gmail.com>
2024-06-14 10:54:48 +0200

    fix: inject var env script

--- injectVarEnv.sh
+++ injectVarEnv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Replace placeholders in JavaScript files
-find /usr/share/nginx/html/assets -name '*.js' -exec sed -i 's|__VITE_API_URL__|'${VITE_API_URL}'|g' {} \;
-find /usr/share/nginx/html/assets -name '*.js' -exec sed -i 's|__VITE_RADARR_URL__|'${VITE_RADARR_URL}'|g' {} \;
+find /usr/share/nginx/html/assets -name '*.js' -exec sed -i 's|__DOCKER_COMPOSE_API_URL__|'${API_URL}'|g' {} \;
+find /usr/share/nginx/html/assets -name '*.js' -exec sed -i 's|__DOCKER_COMPOSE_RADARR_URL__|'${RADARR_URL}'|g' {} \;
